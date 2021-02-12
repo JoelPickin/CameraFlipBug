55f8cde9b8fa0ab015ed6b088a15010af91c5289
Joel Pickin <joelpickin@outlook.com>
2021-02-12 11:33:09 +0000

    Add project files.

---
+++ CameraFlipBug/CameraFlipBug/LandscapeContentView.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CameraFlipBug
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LandscapeContentView : ContentView
+    {
+        public LandscapeContentView()
+        {
+            InitializeComponent();
+        }
+
+        void CameraView_OnAvailable(object sender, bool e)
+        {
+            captureButton.IsEnabled = e;
+        }
+
+        async void CameraView_MediaCaptured(object sender, MediaCapturedEventArgs e)
+        {
+            switch (cameraView.CaptureMode)
+            {
+                default:
+                case CameraCaptureMode.Default:
+                    shutterFeedbackPage.IsVisible = true;
+
+                    break;
+            }
+
+            await shutterFeedbackPage.FadeTo(1, 150);
+
+            shutterFeedbackPage.IsVisible = false;
+        }
+
+        private void EnableDisableFlash_Tapped(object sender, EventArgs e)
+        {
+            if (cameraView.FlashMode == CameraFlashMode.Off)
+            {
+                cameraView.FlashMode = CameraFlashMode.On;
+                flashButton.TextColor = Color.Yellow;
+            }
+            else if (cameraView.FlashMode == CameraFlashMode.On)
+            {
+                cameraView.FlashMode = CameraFlashMode.Off;
+                flashButton.TextColor = Color.White;
+            }
+        }
+
+        double currentScale = 1;
+        double startScale = 1;
+
+        private void PinchGestureRecognizer_PinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+        {
+            if (e.Status == GestureStatus.Started)
+            {
+                //Captured cameras current zoom
+                startScale = cameraView.Zoom;
+            }
+            if (e.Status == GestureStatus.Running)
+            {
+                //Calculate the scale factor to be applied.
+                currentScale += (e.Scale - 1) * startScale;
+                currentScale = Math.Max(1, currentScale);
+
+                //Apply scale factor
+                cameraView.Zoom = currentScale;
+            }
+        }
+    }
+}
---
+++ CameraFlipBug/CameraFlipBug/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+
+namespace CameraFlipBug
+{
+    public partial class MainPage : ContentPage
+    {
+        private PortraitContentView _portrait;
+        private LandscapeContentView _landscape;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+		}
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            if (width > height)
+            {
+                Content = GetLandscapeView();
+            }
+            else
+            {
+                Content = GetPortraitView();
+            }
+        }
+
+        private View GetPortraitView()
+        {
+            if (_portrait == null)
+            {
+                _portrait = new PortraitContentView();
+            }
+
+            return _portrait;
+        }
+
+        private View GetLandscapeView()
+        {
+            if (_landscape == null)
+            {
+                _landscape = new LandscapeContentView();
+            }
+
+            return _landscape;
+        }
+    }
+}
